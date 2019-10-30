6e1344f455dd4e4f7d52c54f9980d530ec41032e
Eduardo Rothe <eduardo-externe.rothe@edf.fr>
2019-10-30 20:19:35 +0100

    PEP8: apply compliance with
     - ubench/scheduler_interfaces/slurm_interface.py

--- ubench/scheduler_interfaces/slurm_interface.py
+++ ubench/scheduler_interfaces/slurm_interface.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 ##############################################################################
 #  This file is part of the UncleBench benchmarking tool.                    #
@@ -18,117 +17,120 @@
 #  along with UncleBench.  If not, see <http://www.gnu.org/licenses/>.       #
 #                                                                            #
 ##############################################################################
+# pylint: disable=no-self-use
+""" Provides SlurmInterface class"""
 
-import pyslurm
-import re
-from ClusterShell.NodeSet import NodeSet
-from subprocess import call, Popen, PIPE
+
 import os
-class SlurmInterface:
-
-     def __init__(self):
-          """ Constructor """
-#          self.partition_dic{}=
-#          a = pyslurm.partition()
-#          part_dict = a.get()
-#          if len(part_dict) > 0:
-#               for key, value in part_dict.iteritems():
-#                    self.partition_list.append(value['nodes'])
-#          print self.partition_list
-                    # print "%s :" % key
-                    # for part_key in sorted(value.iterkeys()):
-                     #     if part_k
-                     #     if part_key in date_fields:
-                         #      ddate = gmtime(value[part_key])
-                          #     ddate = strftime("%a %b %d %H:%M:%S %Y", ddate)
-                      #         print "\t%-20s : " % (part_key)
-                      #    else:
-                       #        print "\t%-20s : %s" % (part_key, value[part_key])
-                       #        print "-" * 80
+from subprocess import Popen, PIPE
+from ClusterShell.NodeSet import NodeSet
+import pyslurm  # pylint: disable=import-error
 
 
+class SlurmInterface(object):
+    """ Provides methods to execute jobs with slurm scheduler """
 
-     def get_available_nodes(self,slices_size=1):
-          """ Returns a list of currently available nodes by slice of slices_size
-          ex: for slices of size 4 ['cn[100-103]','cn[109,150-152]']
-          :param slices_size: slices size
-          :param type: int
-          :returns: list of nodes_id
-          :rtype: str """
-
-          node_list=[]
-          a = pyslurm.node()
-          node_dict = a.get()
-          node_count=0
-          nodeset = NodeSet()
-          if len(node_dict) > 0:
-               for key, value in sorted(node_dict.items()):
-                    if value['state']=='IDLE':
-                         nodetype=value
-                         nodeset.update(key)
-                         node_count+=1
-                    if node_count==slices_size:
-                         node_list.append(str(nodeset))
-                         nodeset=NodeSet()
-                         slice_str=None
-                         node_count=0
+    def __init__(self):
+        """ Constructor """
 
 
-          return node_list
+    def get_available_nodes(self, slices_size=1):
+        """ Returns a list of currently available nodes by slice of slices_size
+        ex: for slices of size 4 ['cn[100-103]','cn[109,150-152]']
 
-     def get_truncated_nodes_lists(self,nnodes_list,nodes_id):
-          """ From a list of nodes number and a list of nodes id returns a list of nodes_id
-          truncated according to nodes number
-          :param nnodes_list: ex [2,4]
-          :type nnodes_list: list of int
-          :param nodes_id: ex ['cn[100-104]','cn[50-84]']
-          :type nodes_id: list of str
-          :returns: truncated node list ex: ['cn[100-101]','cn[50-53]']
-          :rtype: list of str
-          """
-          nodes_id_list=[]
-          for nnode in nnodes_list :
-               nodeset=NodeSet()
-               nodeset.update(nodes_id)
-               if nnode>len(nodeset):
-                    raise Exception('Number of nodes is greater than the giver number of nodes id')
-               nodes_id_list.append(str(nodeset[:nnode]))
+        Args:
+            (int) slices_size: slices size
+
+        Returns:
+            (str) list of nodes_id
+        """
+
+        node_list = []
+        a = pyslurm.node()  # pylint: disable=invalid-name
+        node_dict = a.get()
+        node_count = 0
+        nodeset = NodeSet()
+
+        if len(node_dict) > 0:
+            for key, value in sorted(node_dict.items()):
+
+                if value['state'] == 'IDLE':
+                    nodetype = value  # pylint: disable=unused-variable
+                    nodeset.update(key)
+                    node_count += 1
+
+                if node_count == slices_size:
+                    node_list.append(str(nodeset))
+                    nodeset = NodeSet()
+                    slice_str = None  # pylint: disable=unused-variable
+                    node_count = 0
+
+        return node_list
 
 
-          return nodes_id_list
+    def get_truncated_nodes_lists(self, nnodes_list, nodes_id):
+        """ From a list of nodes number and a list of nodes id returns a list of nodes_id
+        truncated according to nodes number
 
-     def get_nnodes_from_string(self,nodes_id):
-          """ From a string reprenting a set of nodes returns the number of nodes
-          the set contains.
-          :param nodes_id: nodes id
-          :type nodes_id: str
-          :returns: number of nodes
-          :rtype: int
-          """
-          nodeset=NodeSet(nodes_id)
-          return len(nodeset)
+        Args:
+            (list of int) nnodes_list: ex [2,4]
+            (list of str) nodes_id: ex ['cn[100-104]','cn[50-84]']
 
-     def get_job_info(self,job_id):
-         """Return a hash with job information using an id
-         :param job_id: job id
-         :type job_id: int
-         :returns: hash with job information
-         :rtype: hash
-         """
+        Returns:
+            (list of str) truncated node list ex: ['cn[100-101]','cn[50-53]']
+        """
+        nodes_id_list = []
+        for nnode in nnodes_list:
+            nodeset = NodeSet()
+            nodeset.update(nodes_id)
+            if nnode > len(nodeset):
+                raise Exception('Number of nodes is greater than the giver number of nodes id')
+            nodes_id_list.append(str(nodeset[:nnode]))
 
-         job_cmd ="sacct --jobs={0} -n -p --format=JobName,Elapsed,NodeList,Submit,Start".format(job_id)
-         job_process = Popen(job_cmd,cwd=os.getcwd(),shell=True, stdout=PIPE, universal_newlines=True)
-         job_info=[]
-         for line in job_process.stdout:
-           fields = line.split("|")
-           job_name = fields[0]
-           job_info_temp = {}
-           if job_name != "batch":
-             job_info_temp['job_name'] = job_name
-             job_info_temp['job_elasped'] = fields[1]
-             job_info_temp['job_nodelist'] = [node for node in NodeSet(fields[2])]
-             job_info_temp['job_submit_time'] = fields[3]
-             job_info_temp['job_start_time'] =fields[4]
-             job_info.append(job_info_temp)
+        return nodes_id_list
 
-         return job_info
+
+    def get_nnodes_from_string(self, nodes_id):
+        """ From a string reprenting a set of nodes returns the number of nodes
+        the set contains.
+
+        Args:
+            (str) nodes_id: nodes id
+
+        Returns:
+            (int) number of nodes
+        """
+
+        nodeset = NodeSet(nodes_id)
+        return len(nodeset)
+
+
+    def get_job_info(self, job_id):
+        """Return a hash with job information using an id
+
+        Args:
+            (int) job_id: job id
+
+        Returns:
+            (dictionary) Job information
+        """
+
+        job_cmd = ('sacct --jobs={0} -n -p --format=JobName,Elapsed,NodeList,Submit,Start'
+                   .format(job_id))
+
+        job_process = Popen(job_cmd, cwd=os.getcwd(), shell=True,
+                            stdout=PIPE, universal_newlines=True)
+        job_info = []
+        for line in job_process.stdout:
+            fields = line.split("|")
+            job_name = fields[0]
+            job_info_temp = {}
+            if job_name != 'batch':
+                job_info_temp['job_name'] = job_name
+                job_info_temp['job_elasped'] = fields[1]
+                job_info_temp['job_nodelist'] = [node for node in NodeSet(fields[2])]
+                job_info_temp['job_submit_time'] = fields[3]
+                job_info_temp['job_start_time'] = fields[4]
+                job_info.append(job_info_temp)
+
+        return job_info
