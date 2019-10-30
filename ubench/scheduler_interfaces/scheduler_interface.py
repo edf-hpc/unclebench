9b3be26c12aa38ac9c5cc8cb06a2a08642420549
Eduardo Rothe <eduardo-externe.rothe@edf.fr>
2019-10-30 20:19:16 +0100

    PEP8: apply compliance with
     - ubench/scheduler_interfaces/scheduler_interface.py

--- ubench/scheduler_interfaces/scheduler_interface.py
+++ ubench/scheduler_interfaces/scheduler_interface.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 ##############################################################################
 #  This file is part of the UncleBench benchmarking tool.                    #
-#        Copyright (C) 2017  EDF SA                                          #
+#        Copyright (C) 2019 EDF SA                                           #
 #                                                                            #
 #  UncleBench is free software: you can redistribute it and/or modify        #
 #  it under the terms of the GNU General Public License as published by      #
@@ -11,49 +10,62 @@
 #                                                                            #
 #  UncleBench is distributed in the hope that it will be useful,             #
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              #
 #  GNU General Public License for more details.                              #
 #                                                                            #
 #  You should have received a copy of the GNU General Public License         #
-#  along with UncleBench.  If not, see <http://www.gnu.org/licenses/>.       #
+#  along with UncleBench. If not, see <http://www.gnu.org/licenses/>.        #
 #                                                                            #
 ##############################################################################
+""" Provides SchedulerInterface class """
 
 
-class SchedulerInterface:
-    
-     def __init__(self):
-          """ Constructor """
+class SchedulerInterface(object):
+    """
 
-     def get_available_nodes(self,slices_size=1):
-          """ Returns a list of currently available nodes by slice of slices_size
-          ex: for slices of size 4 ['cn[100-103]','cn[109,150-152]'].
-          :param slices_size: slices size
-          :param type: int
-          :returns: list of nodes_id 
-          :rtype: str """
-          pass          
+    Methods:
+        get_available_nodes(slices_size=1)
+        get_truncated_nodes_lists(nnodes_list, nodes_id)
+        get_nnodes_from_string(nodes_id)
+    """
 
-     def get_truncated_nodes_lists(self,nnodes_list,nodes_id):
-          """ From a list of nodes number and a list of nodes id returns a list of nodes_id 
-          truncated according to nodes number.
-          :param nnodes_list: ex [2,4]
-          :type nnodes_list: list of int
-          :param nodes_id: ex ['cn[100-104]','cn[50-84]']
-          :type nodes_id: list of str
-          :returns: truncated node list ex: ['cn[100-101]','cn[50-53]']
-          :rtype: list of str
-          """
-          pass
 
-     def get_nnodes_from_string(self,nodes_id):
-          """ From a string reprenting a set of nodes returns the number of nodes
-          the set contains.
-          :param nodes_id: nodes id
-          :type nodes_id: str
-          :returns: number of nodes  
-          :rtype: int
-          """
-          pass
+    def __init__(self):
+        """ Constructor """
 
-     
+
+    def get_available_nodes(self, slices_size=1):
+        """ Returns a list of currently available nodes by slice of slices_size
+        ex: for slices of size 4 ['cn[100-103]','cn[109,150-152]'].
+
+        Args:
+            (int) slices_size: slices size
+        """
+        pass
+
+
+    def get_truncated_nodes_lists(self, nnodes_list, nodes_id):
+        """ From a list of nodes number and a list of nodes id returns a list of nodes_id
+        truncated according to nodes number.
+
+        Args:
+            (list of int) nnodes_list: ex [2,4]
+            (list of str) nodes_id: ex ['cn[100-104]','cn[50-84]']
+
+        Returns:
+            (list of str) Truncated node list ex: ['cn[100-101]','cn[50-53]']
+        """
+        pass
+
+
+    def get_nnodes_from_string(self, nodes_id):
+        """ From a string reprenting a set of nodes returns the number of nodes
+        the set contains.
+
+        Args:
+            (str) nodes_id: nodes id
+
+        Returns:
+            (int) number of nodes
+        """
+        pass
