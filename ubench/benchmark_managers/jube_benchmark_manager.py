188f9a77a1a00aae08a7bb58acb0ebff6312f3e4
Eduardo Rothe <eduardo-externe.rothe@edf.fr>
2019-10-30 17:25:04 +0100

    PEP8: apply compliance with
     - ubench/benchmark_managers/jube_benchmark_manager.py

--- ubench/benchmark_managers/jube_benchmark_manager.py
+++ ubench/benchmark_managers/jube_benchmark_manager.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 ##############################################################################
 #  This file is part of the UncleBench benchmarking tool.                    #
-#        Copyright (C) 2017  EDF SA                                          #
+#        Copyright (C) 2019 EDF SA                                           #
 #                                                                            #
 #  UncleBench is free software: you can redistribute it and/or modify        #
 #  it under the terms of the GNU General Public License as published by      #
@@ -11,30 +10,33 @@
 #                                                                            #
 #  UncleBench is distributed in the hope that it will be useful,             #
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              #
 #  GNU General Public License for more details.                              #
 #                                                                            #
 #  You should have received a copy of the GNU General Public License         #
-#  along with UncleBench.  If not, see <http://www.gnu.org/licenses/>.       #
+#  along with UncleBench. If not, see <http://www.gnu.org/licenses/>.        #
 #                                                                            #
 ##############################################################################
-"""
-Define JubeBenchmarkManager class.
-"""
+""" Define JubeBenchmarkManager class. """
+
+
 import ubench.benchmarking_tools_interfaces.jube_benchmarking_api as jba
 import ubench.benchmark_managers.standard_benchmark_manager as stdbm
 
+
 class JubeBenchmarkManager(stdbm.StandardBenchmarkManager):
-    """
-    Concrete class defining an interface to run jube benchmarks and
+    """ Concrete class defining an interface to run jube benchmarks and
     manage their results.
     """
+
     def __init__(self, benchmark_name, platform, uconf):
+        """ Class constructor """
+
         self.platform = platform
-        stdbm.StandardBenchmarkManager.__init__(self,benchmark_name, platform, uconf)
+        stdbm.StandardBenchmarkManager.__init__(self, benchmark_name, platform, uconf)
+
 
     def get_benchmarking_api(self):
-        """
-        Factory method to get a new JubeBenchmarkingAPI
-        """
+        """ Factory method to get a new JubeBenchmarkingAPI """
+
         return jba.JubeBenchmarkingAPI(self.benchmark_name, self.platform)
