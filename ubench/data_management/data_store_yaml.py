f50f1d7d4db73cb42a9ef98419686143dd7539f2
Eduardo Rothe <eduardo-externe.rothe@edf.fr>
2019-10-30 20:34:26 +0100

    PEP8: apply compliance with
     - ubench/data_management/data_store_yaml.py

--- ubench/data_management/data_store_yaml.py
+++ ubench/data_management/data_store_yaml.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 ##############################################################################
 #  This file is part of the UncleBench benchmarking tool.                    #
-#        Copyright (C) 2017  EDF SA                                          #
+#        Copyright (C) 2019 EDF SA                                           #
 #                                                                            #
 #  UncleBench is free software: you can redistribute it and/or modify        #
 #  it under the terms of the GNU General Public License as published by      #
@@ -10,42 +9,68 @@
 #                                                                            #
 #  UncleBench is distributed in the hope that it will be useful,             #
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              #
 #  GNU General Public License for more details.                              #
 #                                                                            #
 #  You should have received a copy of the GNU General Public License         #
-#  along with UncleBench.  If not, see <http://www.gnu.org/licenses/>.       #
+#  along with UncleBench. If not, see <http://www.gnu.org/licenses/>.        #
 #                                                                            #
 ##############################################################################
+""" Provides DataStoreYAML class """
 
-from . import data_store
+
 import yaml
-from collections import OrderedDict
+from . import data_store
+
 
 class DataStoreYAML(data_store.DataStore):
+    """ Provides methods to load and write configuration from and to YAML files
+
+    Methods:
+        write(metadata, runs_info, output_file)
+        load(input_file)
     """
-    Load Yaml to dictionnary / write dictionnary to Yaml files.
-    """
+
+
     def __init__(self):
+        """ Class constructor """
         data_store.DataStore.__init__(self)
 
+
     def write(self, metadata, runs_info, output_file):
+        """
+
+        Args:
+            metadata
+            runs_info
+            output_file
+        """
+
         with open(output_file, 'w') as outfile:
-          benchdata = metadata
-          benchdata['runs'] = runs_info
-          yaml.dump(benchdata, outfile, default_flow_style=False)
+            benchdata = metadata
+            benchdata['runs'] = runs_info
+            yaml.dump(benchdata, outfile, default_flow_style=False)
+
 
     def load(self, input_file):
-        metadata=None
-        runs_info=None
+        """ Loads YAML configuration to dictionary
+
+        Args:
+            input_file (str): YAML file
+        """
+
+        metadata = None
+        runs_info = None
+
         with open(input_file, 'r') as inputfile:
             try:
                 data = yaml.load(inputfile)
                 runs_info = data['runs']
-                data.pop('runs',None)
+                data.pop('runs', None)
                 metadata = data
-            except Exception as e:
+            except Exception as e:  # pylint: disable=invalid-name, broad-except, unused-variable
                 data = None
                 runs_info = None
                 metadata = None
-        return(metadata,runs_info)
+
+        return(metadata, runs_info)
