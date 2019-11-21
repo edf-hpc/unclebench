ea41f66c0b1877b5638c140b9040d9d7abdfe7e5
Cristian Ruiz <cristian-externe.ruiz@edf.fr>
2019-11-21 11:25:08 +0100

    Add tests for ubench_commands class

---
+++ tests/test_ubench_commands.py
@@ -0,0 +1,59 @@
+##############################################################################
+#  This file is part of the UncleBench benchmarking tool.                    #
+#        Copyright (C) 2019 EDF SA                                           #
+#                                                                            #
+#  UncleBench is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by      #
+#  the Free Software Foundation, either version 3 of the License, or         #
+#  (at your option) any later version.                                       #
+#                                                                            #
+#  UncleBench is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              #
+#  GNU General Public License for more details.                              #
+#                                                                            #
+#  You should have received a copy of the GNU General Public License         #
+#  along with UncleBench. If not, see <http://www.gnu.org/licenses/>.        #
+#                                                                            #
+##############################################################################
+""" Provides test API """
+
+# pylint: disable=unused-import,unused-variable,line-too-long
+import pytest
+import mock
+import pytest_mock
+import ubench.core.ubench_commands as ubench_commands
+
+def test_wlist():
+    """ docstring """
+
+    cmd = ubench_commands.UbenchCmd("platform", [])
+    assert cmd.translate_wlist_to_scheduler_wlist(['1', '2', '3']) == [(1, None), (2, None), (3, None)]
+    assert cmd.translate_wlist_to_scheduler_wlist(['6', 'cn184', 'cn[380,431-433]']) == [(6, None), (1, 'cn184'), (4, 'cn[380,431-433]')]
+    assert cmd.translate_wlist_to_scheduler_wlist(['6', 'cn184', 'host1']) == [(6, None), (1, 'cn184'), (1, 'host1')]
+
+def test_run_noresourcedir(mocker):
+    """ docstring """
+
+    mock_bms = mocker.patch("ubench.benchmark_managers.benchmark_manager_set.BenchmarkManagerSet.run")
+
+    cmd = ubench_commands.UbenchCmd("platform", [])
+
+    assert cmd.run({'w':[]}) is False
+
+def test_run_withresourcedir(mocker):
+    """ docstring """
+
+    mock_bms = mocker.patch("ubench.benchmark_managers.benchmark_manager_set.BenchmarkManagerSet.run")
+    mock_isdir = mocker.patch("os.path.isdir")
+    cmd = ubench_commands.UbenchCmd("platform", [])
+
+    assert cmd.run({'w':[], 'file_params' :[], 'custom_params' : []}) is True
+    mock_bms.assert_called_with("platform", {'w':[],
+                                             'file_params' :[],
+                                             'custom_params' : []})
+
+    cmd.run({'w':['6', 'cn184', 'cn[380,431-433]'], 'file_params' :[], 'custom_params' : []})
+    mock_bms.assert_called_with("platform", {'w':[(6, None), (1, 'cn184'), (4, 'cn[380,431-433]')],
+                                             'file_params' :[],
+                                             'custom_params' : []})
