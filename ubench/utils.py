e99b55220cff8e14606448098cf9b097e65333da
Eduardo Rothe <eduardo-externe.rothe@edf.fr>
2020-05-04 14:56:30 +0200

    utils.py : adds general utility functions
    get_bench_rundir : returns bench run directory up to the bench id
    clean_list : removes entries not matching 6 digit strings in list

--- ubench/utils.py
+++ ubench/utils.py
@@ -50,3 +50,54 @@ def run_cmd_bg(cmd_string, cwd, env=None):
                 universal_newlines=True)
 
     return cmd
+
+def get_bench_rundir(base_path, platform, benchmark, benchmark_id=None, fill=6):
+    ''' Returns benchmark running directory up to the benchmark id
+
+    If no benchmark_id is specified, returns the path to the last
+    executed benchmark. It uses a base_path argument which prevents
+    the loading of unclebench related classes (UbenchConfig in this case).
+
+    The need for such funcionality is now beyond the scope of the
+    execution of the benchmark itself and therefore it makes sense to
+    have this function implemented outside JubeBenchmarkingAPI class.
+    In future releases this functionality could cease to exist in
+    JubeBenchmarkinkAPI class with small refactoring.
+
+    Args:
+        base_path (string)
+        platform (string)
+        benchmark (string)
+        benchmark_id (integer, optional)
+    Returns:
+        string
+    Example:
+        `get_bench_rundir(base_path, ironman, hpl)` will return
+        'base_path/ironman/hpl/benchmarkruns/000007' if 7 is
+        the total number of hpl benchmarks executed in ironman.
+    '''
+    abs_path = os.path.join(base_path, platform, benchmark, 'benchmark_runs')
+
+    if os.path.isdir(abs_path):
+        if benchmark_id is None:
+            dir_list = clean_list(os.listdir(abs_path))
+            path_id = max(dir_list)
+        else:
+            path_id = benchmark_id
+    else:
+        print 'Could not find benchmarks directory'
+        exit(1)
+
+    return os.path.join(abs_path, str(path_id).zfill(fill))
+
+def clean_list(dir_list):
+    ''' Removes entries not matching six 6 digit strings
+
+    Args:
+        list of strings
+    Returns:
+        list of strings
+    Example:
+        clean_list(['000111', 'aaabcd'] -> ['000111']
+    '''
+    return [x for x in dir_list if re.match(r'[0-9]{6}$', x) is not None]
